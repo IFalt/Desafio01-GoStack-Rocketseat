23c2f6ae247fac1015deac461202fd8d37ffa6ad
Juliano Krindges <juliano9krindges@hotmail.com>
2019-08-24 19:36:03 -0300

    feat: delete projects and add tasks - add midd

--- index.js
+++ index.js
@@ -6,11 +6,37 @@ app.use(express.json());
 
 const projects = []
 
+let NumRequires = 0
+
+/* Middlewares */
+
+app.use((req, res, next) => {
+  NumRequires++
+
+  console.log(`${NumRequires} Request made `)
+
+  next();
+})
+
+function CheckProjectExists (req, res, next) {
+  const {id} = req.params
+  const project = projects.find(param => param.id == id)
+  console.log(project)
+
+  if (!project){
+    return res.status(400).json({ error: "Project does not exists"})
+  }
+
+  return next()
+}
+
+/* Projects Routes */
+
 app.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
-app.post("/projects", (req, res) => {
+app.post('/projects', (req, res) => {
   const { id, title } = req.body;
 
   const project = {
@@ -24,8 +50,8 @@ app.post("/projects", (req, res) => {
   return res.json(project);
 });
 
-app.put('/projects/:id', (req,res)=>{
-  const id = req.params.id
+app.put('/projects/:id', CheckProjectExists, (req,res)=>{
+  const {id} = req.params
   const {title}=req.body
 
   const project = projects.find(param => param.id == id)
@@ -34,5 +60,29 @@ app.put('/projects/:id', (req,res)=>{
   
   return res.json(projects)
 })
+
+app.delete('/projects/:id', CheckProjectExists, (req, res) =>{
+  const {id} = req.params
+
+  const IndexProject = projects.map(param => param.id).indexOf(id)
+
+  projects.splice(IndexProject, 1)
+
+  return res.json(projects)
+
+})
+
+/* Tasks */
+
+app.post('/projects/:id/tasks', CheckProjectExists, (req, res) => {
+  const {id}= req.params
+  const { title } = req.body;
+
+  const project = projects.find(param => param.id == id)
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
 
 app.listen(3333);
